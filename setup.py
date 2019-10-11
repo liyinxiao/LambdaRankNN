2866d8045a093a24238e59f992a4e63a91bacd26
liyinxiao <liyinxiao1227@gmail.com>
2019-10-11 10:54:32 -0400

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LambdaRankNN",
-    version="0.1.0",
+    version="0.1.1",
     author="Yinxiao Li",
     author_email="liyinxiao1227@gmail.com",
     description="LambdaRank Neural Netwrok model using Keras.",
